645fc1aa13e550d7ef61ed8972caacea110806ea
Zachary Drummond <zdrummond@users.noreply.github.com>
2023-12-27 13:36:18 -0800

    Basic of DB setup

--- project/app/main.py
+++ project/app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 
+from app.db import init_db
+from app.models import Song
+
 app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup():
+    init_db()
 
 
 @app.get("/ping")
