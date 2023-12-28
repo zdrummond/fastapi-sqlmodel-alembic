5f22ad9c1ba3c2cf87eb9459df98125db08638d6
Zachary Drummond <zdrummond@users.noreply.github.com>
2023-12-28 14:48:32 -0800

    Comment out unused init db

--- project/app/db.py
+++ project/app/db.py
@@ -3,14 +3,15 @@ import os
 from sqlmodel import create_engine, SQLModel, Session
 
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL: str = os.environ.get("DATABASE_URL", "")
 
 engine = create_engine(DATABASE_URL, echo=True)
 
 
-def init_db():
-    # This creates [both db and tables](https://sqlmodel.tiangolo.com/tutorial/fastapi/simple-hero-api/#create-database-and-tables-on-startup)
-    SQLModel.metadata.create_all(engine)
+# def init_db():
+#     # This creates [both db and tables](https://sqlmodel.tiangolo.com/tutorial/fastapi/simple-hero-api/#create-database-and-tables-on-startup)
+#     # Not needed if you are using Alembic
+#     SQLModel.metadata.create_all(engine)
 
 
 def get_session():
